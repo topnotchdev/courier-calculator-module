017b757ce5e2a5d2fd0b54f362844891ab66d82e
Kofi Richardson <sybergremlin@hotmail.com>
2025-03-27 22:16:49 +0000

    [main] Added Unit test skelton

---
+++ tests/modules/Unit/DeliveryJobTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Modules\Unit;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\TestCase;
+
+class DeliveryJobTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_can_calculate_total_cost_of_delivery_job(): void
+    {
+
+    }
+}
